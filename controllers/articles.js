015a6e4036cc1a0865b4614eddf07437e666d36b
Nathaniel Dominguez <nathaniel.a.dominguez@gmail.com>
2018-08-20 17:37:20 -0700

    added new views for articles and authors

--- controllers/articles.js
+++ controllers/articles.js
@@ -2,7 +2,19 @@ var express = require('express')
 var router = express.Router();
 
 router.get('/', function(req, res) {
-	res.send('HyAH HYRULE articles');
+	res.render('articles/index');
+});
+
+router.get('/new', function(req, res) {
+	res.render('articles/new');
+});
+
+router.get('/:id', function(req, res) {
+	res.send('article show page goes here');
+});
+
+router.post('/', function(req, res) {
+	res.send('/articles POST route reached')
 });
 
 module.exports = router;
